e848a51973e095d1df0dc8f610975e960a9238e8
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-24 22:32:28 +0530

    mongoose connected

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose')
 
 const URI = `mongodb+srv://${process.env.NAME}:${process.env.DPASS}@cluster0.1tdiu.mongodb.net/${process.env.dbname}`
 const apiPage = require('./module/feed');
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api',apiPage);
 // app.use('/auth',authPage);
 
-app.listen(8080,()=>{
-    console.log('listening on http://localhost:8080');
+mongoose.connect(URI)
+.then(()=>{
+    app.listen(8080,()=>{
+        console.log('listening on http://localhost:8080');
+    })
 })
+.catch(err=>console.log(err))
