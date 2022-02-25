df7d20d874bd0799d249360140a4f2000fe7c9a8
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-25 10:32:54 +0530

    validation added

--- module/feed.js
+++ module/feed.js
@@ -1,11 +1,26 @@
 const route = require('express').Router();
-
+const {body,validationResult} = require('express-validator/check')
 
 const Data = require('../models/data')
 
-route.use('/data',(req,res,next)=>{
+route.post('/data',
+[
+    body('marks').not().isEmpty().withMessage('Requires Mark'),
+    body('status').not().isEmpty().withMessage('Requires Econmical Stability'),
+    body('personality').not().isEmpty().withMessage('Requires Personality'),
+    body('agree').not().isEmpty().withMessage('Requires Agreeableness'),
+    body('opens').not().isEmpty().withMessage('Requires Openness'),
+    body('aoi').not().isEmpty().withMessage('Requires Area Of Interest'),
+],
+(req,res,next)=>{
+    console.log(req.body)
+    res.status(500).json({data:'success'})
+})
+
+route.get('/data',(req,res,next)=>{
     console.log(req.body)
     res.status(500).json({data:req.body.title})
 })
+
 
 module.exports=route
