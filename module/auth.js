281c6b6a382bb1f54486694e9da37d563f412fb0
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-25 22:45:20 +0530

    signup route

--- module/auth.js
+++ module/auth.js
@@ -1,11 +1,46 @@
 const route = require('express').Router();
 
+const bcrypt = require('bcrypt')
+
 const User = require('../models/user')
 
 
 
 route.use('/login',(req, res,next)=>{
     res.status(200).json({message:'Working Good'})
+})
+
+route.post('/signup',(req, res,next)=>{
+    console.log(req.body)
+    User.findOne({name:req.body.name})
+    .then((result)=>{
+        console.log(result)
+        if(result){
+            const error = new Error('User Already Exist')
+            error.statusCode = 442;
+            throw error
+        }
+        bcrypt.hash(req.body.password,12)
+        .then((hash)=>{
+            const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password:hash
+            })
+            return user.save()
+        })
+        .then(()=>{
+            res.status(201).json({message:'User Signed Up Successfully'})
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+        if(!err.statusCode){
+            err.statusCode = 442
+        }
+        next(err)
+    })
+    
 })
 
 
