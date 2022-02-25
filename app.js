1bbf7eb1d3686aa2a90d0fac82335420e51d4e12
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-25 12:58:18 +0530

    CORS handled

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const authPage = require('./module/auth');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    next()
+})
 app.use('/api',apiPage);
 app.use('/auth',authPage);
 
