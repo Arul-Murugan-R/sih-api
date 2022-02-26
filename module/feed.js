b6fa6f3cb1bc5d339216dabf3aaa793954dc604c
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-26 11:20:58 +0530

    statusCode modified

--- module/feed.js
+++ module/feed.js
@@ -13,13 +13,16 @@ route.post('/data',
     body('aoi').isLength({min:1,max:3}).withMessage('Requires Area Of Interest'),
 ],
 (req,res,next)=>{
+    console.log(req.body)
+    // console.log(JSON.parse(req.body))
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        errors.statusCode = 442;
-        errors.message = 'Data Should be filled Completely'
-        throw errors
+        const error = new Error('Data Should be filled Completely')
+        error.statusCode = 422;
+        error.cons = errors.array()
+        throw error
     }
-    console.log(req.body)
+    // console.log(req.body)
     const data = new Data({
         marks:req.body.marks,
         status:req.body.status,
@@ -35,7 +38,7 @@ route.post('/data',
     .catch(err=>{
         console.log(err)
         if(!err.statusCode){
-            err.statusCode = 442
+            err.statusCode = 422
         }
         next(err)
     })
@@ -46,7 +49,7 @@ route.use('/data',(req,res,next)=>{
     .then((dataSet)=>{
         if(!dataSet){
             const error = new Error('No data found in database')
-            error.statusCode = 404;
+            error.statusCode = 401;
             throw error
         }
         console.log(dataSet)
@@ -55,7 +58,7 @@ route.use('/data',(req,res,next)=>{
     .catch(err=>{
         console.log(err)
         if(!err.statusCode){
-            err.statusCode = 442
+            err.statusCode = 422
         }
         next(err)
     })
