a620dc18fa237bf06c6f6e6c6d2a044b32b018f1
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-26 11:19:55 +0530

    token to be added

--- module/auth.js
+++ module/auth.js
@@ -1,23 +1,109 @@
 const route = require('express').Router();
 
+const {body,validationResult} = require('express-validator/check')
 const bcrypt = require('bcrypt')
 
 const User = require('../models/user')
 
+route.post('/login',
+[
+    body('email').not().isEmpty().withMessage('Requires Email').custom((val,{req})=>{
+        return User.findOne({email:val})
+        .then((result)=>{
+            if(!result){
+                return Promise.reject('User Does Not Exist')
+            }
+        })
+    }),
+    body('password').not().isEmpty().withMessage('Password is Empty'),
+],
+(req, res,next)=>{
+    console.log(req.body)
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        const error = new Error('Validation Error')
+        error.cons = errors.array()
+        error.statusCode = 422
+        throw error
+    }
+    User.findOne({email:req.body.email})
+    .then((result)=>{
+        bcrypt.compare(req.body.password, result.password)
+        .then((bool)=>{
+            if(!bool){
+                const error = new Error('Incorrect Password')
+                error.statusCode = 401
+                throw error
+            }
+            res.status(200).json({message:'Working Good',userId:result._id})
 
+        })
+        .catch(err=>{
+            console.log(err)
+            if(!err.statusCode){
+                err.statusCode = 500
+            }
+            next(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+        if(!err.statusCode){
+            err.statusCode = 422
+        }
+        next(err)
+    })
+})
 
 route.use('/login',(req, res,next)=>{
     res.status(200).json({message:'Working Good'})
 })
 
-route.post('/signup',(req, res,next)=>{
+route.post('/signup',
+[
+    body('name').not().isEmpty().withMessage('Requires Name').custom((val,{req})=>{
+        return User.findOne({name:val})
+        .then((result)=>{
+            console.log(result)
+            if(result){
+                return Promise.reject('Please Enter Unique User Name')
+            }
+        })
+    }),
+    body('email').not().isEmpty().withMessage('Requires Email').custom((val,{req})=>{
+        return User.findOne({email:val})
+        .then((result)=>{
+            console.log(result)
+            if(result){
+                return Promise.reject('User Already Exist')
+            }
+        })
+    }),
+    body('password').not().isEmpty().withMessage('Password is Empty'),
+    body('cpassword').not().isEmpty().withMessage('Confirm Password is Empty')
+    
+],
+(req, res,next)=>{
     console.log(req.body)
-    User.findOne({name:req.body.name})
-    .then((result)=>{
-        console.log(result)
-        if(result){
-            const error = new Error('User Already Exist')
-            error.statusCode = 442;
+    const errors = validationResult(req)
+    
+    if(!errors.isEmpty()){
+        const error = new Error('Validation Error')
+        error.cons = errors.array()
+        error.statusCode = 422
+        throw error
+    }
+    // User.findOne({name:req.body.name})
+    // .then((result)=>{
+    //     console.log(result)
+    //     if(result){
+    //         const error = new Error('User Already Exist')
+    //         error.statusCode = 442;
+    //         throw error
+    //     }
+        if(req.body.password != req.body.cpassword){
+            const error = new Error('Both Password Should Be Same')
+            error.statusCode = 401
             throw error
         }
         bcrypt.hash(req.body.password,12)
@@ -27,16 +113,16 @@ route.post('/signup',(req, res,next)=>{
                 email: req.body.email,
                 password:hash
             })
-            return user.save()
-        })
-        .then(()=>{
-            res.status(201).json({message:'User Signed Up Successfully'})
+        user.save()
+        // })
+        .then((result)=>{
+            res.status(201).json({message:'User Signed Up Successfully',userId:result._id})
         })
     })
     .catch(err=>{
         console.log(err)
         if(!err.statusCode){
-            err.statusCode = 442
+            err.statusCode = 422
         }
         next(err)
     })
